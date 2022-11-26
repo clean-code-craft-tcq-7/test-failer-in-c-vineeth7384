8aade05981ef4b2ebbfce06cf5bcf1d7aab212b9
vineeth7384 <67726924+vineeth7384@users.noreply.github.com>
2022-11-26 15:57:11 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,21 +1,41 @@
 #include <stdio.h>
 #include <assert.h>
 
+typedef struct{
+    int colorCode;
+    const char *majorColor;
+    const char *minorColor;
+}colorPair;
+
+colorPair getColorCode(int i, int j){
+    colorPair colorPair;
+    colorPair.colorCode = i * 5 + j;
+    colorPair.majorColor = majorColor[i];
+    colorPair.minorColor = minorColor[i];
+    return colorPair;
+}
 int printColorMap() {
-    const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-    const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
+    colorPair colorPair;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            colorPair = getColorCode(i, j);
+            printf("%d | %s | %s\n", colorPair.colorCode, colorPair.majorColor, colorPair.minorColor);
         }
     }
     return i * j;
 }
 
+
 int main() {
+       colorPair colorPair;
     int result = printColorMap();
     assert(result == 25);
     printf("All is well (maybe!)\n");
+    colorPair = getColorCode(0, 1);
+    assert(colorPair.colorCode == 1);
+    assert(strcmp(colorPair.majorColor, majorColor[0]) == 0);
+    assert(strcmp(colorPair.minorColor, minorColor[1]) == 0);
+
     return 0;
 }
