6c4300addebf8228f3a3fd6b93dc1fec88caa49e
vineeth7384 <67726924+vineeth7384@users.noreply.github.com>
2022-12-07 11:56:16 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,45 +1,57 @@
 #include <stdio.h>
 #include <assert.h>
-#include <string.h> 
+#include <string.h>
+
+#define MAX_COLOR_PAIR 25
+#define MAX_COLOR_LEN 10
+
+typedef struct {
+    char majorColor[MAX_COLOR_LEN];
+    char minorColor[MAX_COLOR_LEN];
+}ColorPair;
 
 const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
 const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+ColorPair colorPairs[MAX_COLOR_PAIR];
 
-typedef struct{
-    int colorCode;
-    const char *majorColor;
-    const char *minorColor;
-}colorPair;
+int getColorMapList() {
 
-colorPair getColorCode(int i, int j){
-    colorPair colorPair;
-    colorPair.colorCode = i * 5 + j;
-    colorPair.majorColor = majorColor[i];
-    colorPair.minorColor = minorColor[i];
-    return colorPair;
-}
-int printColorMap() {
     int i = 0, j = 0;
-    colorPair colorPair;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            colorPair = getColorCode(i, j);
-            printf("%d | %s | %s\n", colorPair.colorCode, colorPair.majorColor, colorPair.minorColor);
+            strcpy(colorPairs[(i*5)+j].majorColor, majorColor[i]);
+            strcpy(colorPairs[(i*5)+j].minorColor, minorColor[j]);
         }
     }
     return i * j;
 }
 
+void printColorMap() {
+    int size, i;
+    size = getColorMapList();
+
+    for (i = 0; i < size; i++) {
+        printf("%d | %s | %s\n", i, colorPairs[i].majorColor, colorPairs[i].minorColor);
+    }
+}
+
 
 int main() {
-    colorPair colorPair;
-    int result = printColorMap();
-    assert(result == 25);
-    printf("All is well (maybe!)\n");
-    colorPair = getColorCode(0, 1);
-    assert(colorPair.colorCode == 1);
-    assert(strcmp(colorPair.majorColor, majorColor[0]) == 0);
-    assert(strcmp(colorPair.minorColor, minorColor[1]) == 0);
 
+    printColorMap();
+
+    assert(strcmp(colorPairs[0].majorColor, "White") == 0);
+    assert(strcmp(colorPairs[0].minorColor, "Blue") == 0);
+    assert(strcmp(colorPairs[1].majorColor, "White") == 0);
+    assert(strcmp(colorPairs[1].minorColor, "Orange") == 0);
+    assert(strcmp(colorPairs[10].majorColor, "Black") == 0);
+    assert(strcmp(colorPairs[10].minorColor, "Blue") == 0);
+    assert(strcmp(colorPairs[11].majorColor, "Black") == 0); 
+    assert(strcmp(colorPairs[20].majorColor, "Violet") == 0);
+    assert(strcmp(colorPairs[20].minorColor, "Blue") == 0);
+    assert(strcmp(colorPairs[24].majorColor, "Violet") == 0);
+    assert(strcmp(colorPairs[24].minorColor, "Slate") == 0);
+
+    printf("All is well (maybe!)\n");
     return 0;
 }
